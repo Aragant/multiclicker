d8648a83d46dbae8d94a4a53b268f7500886c829
Thomas <tlemaire1610@gmail.com>
2025-01-04 17:39:04 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Clicker from "./clicker";
+
+export default function Home() {
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Clicker></Clicker>
+    </div>
+  );
+}
