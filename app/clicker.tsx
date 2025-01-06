a977f3976aa069128648f13cd69b5cc1850c1ed0
KORAÏ <6sistine@protonmail.com>
2025-01-06 22:03:10 +0100

    Premier commit avec les fichiers du projet

---
+++ app/clicker.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useState } from "react";
+
+export default function Clicker() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="flex flex-col items-center gap-8 h-max">
+            <p className="text-4xl">{count}</p>
+            <button
+             onClick={() => setCount(count + 1)}
+             className="text-4xl text-black bg-orange-200 rounded-lg p-4 uppercase">click</button>
+        </div>
+    );
+}
