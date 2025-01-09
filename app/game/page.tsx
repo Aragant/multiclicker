adacabe5b7fb0eecf7a93981488e8d02015397f5
Thomas <tlemaire1610@gmail.com>
2025-01-10 00:09:41 +0100

    add blick with websocket

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,15 +1,36 @@
 'use client';
 
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { WsContext } from "../context/wsContext";
+
 
 export default function Clicker() {
     const [count, setCount] = useState(0);
+    const websocket = useContext(WsContext);
+
+    websocket!.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        console.log(data);
+        setCount(data.sumClick);
+    };
+
+    
+    function bigClick() {
+        console.log('bigclick');
+        setCount(count + 1);
+
+        const event = {
+            type: 'click',
+        }
+
+        websocket!.send(JSON.stringify(event));
+    }
 
     return (
         <div className="flex flex-col items-center justify-center h-full gap-8">
             <p className="text-4xl">{count}</p>
             <button
-             onClick={() => setCount(count + 1)}
+             onClick={bigClick}
              className="text-4xl text-black bg-orange-200 rounded-lg p-4 uppercase">click</button>
         </div>
     );
