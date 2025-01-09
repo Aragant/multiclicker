260b4f6b40ba6d18262b2a0484980dd4439d244d
Thomas <tlemaire1610@gmail.com>
2025-01-09 23:17:20 +0100

    add a basic pseudo login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,36 @@
 'use client';
 
+import { useState, useContext } from "react";
+import { WsContext } from "../context/wsContext";
+import { redirect } from 'next/navigation'
+
+
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const websocket = useContext(WsContext);
+
+
+    const login = async () => {
+        console.log('connected', websocket?.readyState);
+        
+        websocket!.send(JSON.stringify({
+            username: username,
+            type: 'login',
+        }));
+
+        websocket!.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log(data);
+            redirect('/game');
+        };
+    }
+
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            welcome to login page
+            <input className="text-black" 
+            value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="username" />
+            <button onClick={login}>Login</button>
         </div>
     );
 }
