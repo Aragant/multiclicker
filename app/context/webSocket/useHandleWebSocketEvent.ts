c85f41ae23c08545fa9521cf4269ad077d2dd253
Thomas <tlemaire1610@gmail.com>
2025-01-19 02:16:39 +0100

    refacto

--- app/context/webSocket/useHandleWebSocketEvent.ts
+++ app/context/webSocket/useHandleWebSocketEvent.ts
@@ -3,37 +3,61 @@ import { useEffect } from "react";
 import { EventType } from "../eventType";
 import { MulticlickerAction } from "@/app/context/multiclicker/multiclickerReducer";
 
-export const useHandleWebSocketEvent = (websocket: WebSocket | null, dispatch: React.Dispatch<MulticlickerAction>) => {
-    useEffect(() => {
+export const useHandleWebSocketEvent = (
+  websocket: WebSocket | null,
+  dispatch: React.Dispatch<MulticlickerAction>
+) => {
+  useEffect(() => {
+    if (!websocket) {
+      return;
+    }
 
-        if (!websocket) {
-            return;
-        }
+    initWebSocketEvent(websocket, dispatch);
 
-        websocket.onmessage = (event) => {
-            handleMessage(event, dispatch);
-        };
-
-        websocket.onclose = () => console.log("WebSocket fermé.");
-        websocket.onerror = (error) => console.error("WebSocket erreur :", error); 
-
-        return () => {
-            websocket.close();
-        };
-    }, [websocket, dispatch]);
+    return () => {
+      websocket.close();
+    };
+  }, [websocket, dispatch]);
 };
 
+const handleMessage = (
+  event: MessageEvent,
+  dispatch: React.Dispatch<MulticlickerAction>
+) => {
+  const data = JSON.parse(event.data);
 
+  switch (data.type) {
+    case EventType.CLICKED:
+      dispatch({ type: "SET_SUM_SCORE", payload: data.sumScore });
+      dispatch({ type: "SET_PLAYER_SCORE", payload: data.player });
+      break;
+    case EventType.GET_GAME_INFO:
+      dispatch({ type: "GET_GAME_INFO", payload: data.game });
 
-const handleMessage = (event: MessageEvent, dispatch: React.Dispatch<MulticlickerAction>) => {
-    const data = JSON.parse(event.data);
-    if (data.type === EventType.CLICKED) {
-        dispatch({ type: 'SET_SUM_SCORE', payload: data.sumScore });
-        dispatch({ type: 'SET_PLAYER_SCORE', payload: data.player });
-    }
-    if (data.type === EventType.GET_GAME_INFO) {
-        console.log(data);
-        dispatch({ type: 'GET_GAME_INFO', payload: data.game });
-    }
-    console.log("Message reçu : ", data);
+      break;
+  }
+
+  if (data.type === EventType.CLICKED) {
+    dispatch({ type: "SET_SUM_SCORE", payload: data.sumScore });
+    dispatch({ type: "SET_PLAYER_SCORE", payload: data.player });
+  }
+  if (data.type === EventType.GET_GAME_INFO) {
+    console.log(data);
+    dispatch({ type: "GET_GAME_INFO", payload: data.game });
+  }
+
+  console.log("Message reçu : ", data);
 };
+
+function initWebSocketEvent(
+  websocket: WebSocket,
+  dispatch: React.Dispatch<MulticlickerAction>
+) {
+  websocket.onmessage = (event) => {
+    handleMessage(event, dispatch);
+  };
+
+  websocket.onclose = () => console.log("WebSocket fermé.");
+
+  websocket.onerror = (error) => console.error("WebSocket erreur :", error);
+}
