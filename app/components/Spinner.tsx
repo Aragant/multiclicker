a04f6c088a1f2e0c618921ba0e90f7714c4b1103
huggotrico <hauretax@gmail.com>
2025-05-15 21:32:17 +0200

    spiner modification to make it correct with next

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,10 +1,12 @@
+// app/components/Spinner.tsx
 import React from "react";
-import "./spinner.css"; 
+import styles from "./spinner.module.css";
+
 const Spinner: React.FC = () => {
   return (
-    <div className="spinner">
-      <div className="double-bounce1"></div>
-      <div className="double-bounce2"></div>
+    <div className={styles.spinner}>
+      <div className={styles.doubleBounce1}></div>
+      <div className={styles.doubleBounce2}></div>
     </div>
   );
 };
